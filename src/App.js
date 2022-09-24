84d9dafe0d55c77fb3be969c62729af070b59832
tekin <gundogdutekin@gmail.com>
2022-09-24 14:32:57 +0300

    useState usage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import User from "./components/User";
-
+import { useState } from "react";
+//import User from "./components/User";
 function App() {
 
+  const [name,setName]=useState("Tekin");
+  const [age,setAge]=useState(40);
+
   return (
-    <div >
+    <>
+    <h1>Merhaba {name}</h1>
+    <h1>Yaş {age}</h1>
+    <button onClick={()=>setName("Emin Burak")}>Change Name</button>
+    <button onClick={()=>setAge(27)}>Change Age</button>
+    </>
+   /* <div >
        <User adress={{title:'Bağcılar/İstanbul',zip:34495}} name={"Tekin"} surname="Gündoğdu" isloggedIn={true} friends={['Ali','Veli','Deli','Ahmet']}/>
      
-    </div>
+    </div>*/
+
   );
 }
 
