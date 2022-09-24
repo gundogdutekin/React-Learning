cc66cc046b190be20a519a8a53dd2635039d0a42
tekin <gundogdutekin@gmail.com>
2022-09-24 10:50:34 +0300

    Shape definition to props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import User from "./components/User";
 function App() {
   return (
     <div >
-       <User name={"Tekin"} surname="Gündoğdu" isloggedIn={true} friends={['Ali','Veli','Deli','Ahmet']}/>
+       <User adress={{title:'Bağcılar/İstanbul',zip:34495}} name={"Tekin"} surname="Gündoğdu" isloggedIn={true} friends={['Ali','Veli','Deli','Ahmet']}/>
      
     </div>
   );
--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const User=({isloggedIn,name,surname,friends})=>{
+const User=({isloggedIn,name,surname,friends,adress})=>{
     return( 
         <div>
             {isloggedIn ? <h1>{name} {surname}</h1> : <h1>Not Logged In</h1>}
@@ -12,13 +12,18 @@ const User=({isloggedIn,name,surname,friends})=>{
                     
                 }  
             </ul>
+            {adress.title} {adress.zip}
        </div>)
 }
 User.propTypes={
     isloggedIn:PropTypes.bool.isRequired,
     name:PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired,
     surname:PropTypes.string.isRequired,
-    friends:PropTypes.array.isRequired
+    friends:PropTypes.array.isRequired,
+    adress:PropTypes.shape({
+        title:PropTypes.string.isRequired,
+        zip:PropTypes.number.isRequired
+    })
 }
 
 export default User;
