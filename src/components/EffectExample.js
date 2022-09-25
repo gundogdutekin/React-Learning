6b12ceba6c20fe23719ef0393603036c3ad1b805
tekin <gundogdutekin@gmail.com>
2022-09-25 19:32:43 +0300

    How to use useEffect?

---
+++ src/components/EffectExample.js
@@ -0,0 +1,33 @@
+import { useState ,useEffect } from "react";
+
+function EffectExample() {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('Tekin');
+    useEffect(() => { 
+        console.log("Counter state mounted");
+    }, [count]);
+    useEffect(() => { 
+        console.log("Name state mounted");
+    }, [name]);
+  return (
+    <div className="container">
+         <div>
+            <h1>{count}</h1> 
+            <div>
+                 <button className="btn" onClick={()=>setCount(count+1)}>Increase</button> 
+            </div>
+            <hr/>
+            <h1>{name}</h1> 
+            <div>
+                 <button className="btn" onClick={()=>setName("Emin Burak")}>Change Name</button> 
+            </div>
+
+            
+         </div>
+    </div>
+   
+   
+  )
+}
+
+export default EffectExample
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 //import Counter from "./components/Counter";
-import InputExample from "./components/InputExample";
+//import InputExample from "./components/InputExample";
 import reportWebVitals from './reportWebVitals';
+import EffectExample from './components/EffectExample';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <InputExample/>
+    <EffectExample/>
   </React.StrictMode>
 );
 
